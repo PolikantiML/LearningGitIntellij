ddc8838eee8b7d13e4d3a9c8b9356865a7d32b29
PolikantiML <polikantiml@gmail.com>
2023-10-02 01:59:06 +0530

    Commit 2 master test

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,6 +4,8 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("hi and here I am adding more comments");
         System.out.println("and also somthing and something to test the data");
+        System.out.println("This code is to commit");
+        System.out.println("Let us do an another commit");
         System.out.println("A Message from the feature");
         System.out.println("This is alo a message from the feature");
         System.out.println("This code is to commit");
@@ -13,5 +15,9 @@ public class DevClass {
         System.out.println("This is alo a message from the feature");
         System.out.println("hi and here I am adding more comments");
     }
-//by doing this actually I am working on branching and cloning.
+//by doing this actually I am working on branching and cloning
+
+    //adding a new code so that you can commit
+    // so let me push my code into the project wiith a new sysout
+
 }
