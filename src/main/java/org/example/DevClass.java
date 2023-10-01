01247eed7e8bda9ca4eefef530398adef1881b31
Polikanti Goutham Krishna <polikanticse@gmail.com>
2023-10-01 23:41:03 +0530

    Update DevClass.java
    I have added more comments.

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hi and here I am adding more comments");
     }
 //by doing this actually I am working on branching and cloning.
 }
