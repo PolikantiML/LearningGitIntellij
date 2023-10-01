a6fddd26375103c85c4cac80b3ddbbf7c5969cdb
Polikanti Goutham Krishna <polikanticse@gmail.com>
2023-10-01 23:44:27 +0530

    Update DevClass.java
    adding new code

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,6 +3,7 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("hi and here I am adding more comments");
+        System.out.println("and also somthing and something to test the data");
     }
 //by doing this actually I am working on branching and cloning.
 }
