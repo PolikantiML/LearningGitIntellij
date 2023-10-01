445817c44291c027edb7f29d9ff7bf2803654f62
PolikantiML <polikantiml@gmail.com>
2023-10-02 02:01:33 +0530

    Commit 2 feature test

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,13 +4,8 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("hi and here I am adding more comments");
         System.out.println("and also somthing and something to test the data");
-        System.out.println("This code is to commit");
-        System.out.println("Let us do an another commit");
-        System.out.println("I am Comitting after the code has been changed on to the feature");
+        System.out.println("A Message from the feature");
+        System.out.println("This is alo a message from the feature");
     }
-//by doing this actually I am working on branching and cloning
-
-    //adding a new code so that you can commit
-    // so let me push my code into the project wiith a new sysout
-
+//by doing this actually I am working on branching and cloning.
 }
