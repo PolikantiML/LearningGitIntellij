177d521943b57b946f7e960530fe886e64baab99
PolikantiML <polikantiml@gmail.com>
2023-10-02 02:01:33 +0530

    Commit 2 feature test

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,6 +4,8 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("hi and here I am adding more comments");
         System.out.println("and also somthing and something to test the data");
+        System.out.println("A Message from the feature");
+        System.out.println("This is alo a message from the feature");
     }
 //by doing this actually I am working on branching and cloning.
 }
